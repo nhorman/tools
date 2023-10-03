5b8fb2b9fe225a7835e22361b3bd47f2773df0a6
Neil Horman <nhorman@openssl.org>
2023-10-03 16:50:14 -0400

    Adjust measurements for rwlocks
    To avoid biasing the total run time for the rwlock test, separately
    capture the end time of the readers and writers and compute the duration
    and average call time separately
    
    also
    * up the number of iterations to get a better average

--- perf/rwlocks.c
+++ perf/rwlocks.c
@@ -15,15 +15,19 @@
 #include <openssl/crypto.h>
 #include "perflib/perflib.h"
 
-#define NUM_CALLS_PER_BLOCK         100
-#define NUM_CALL_BLOCKS_PER_THREAD  100
+#define NUM_CALLS_PER_BLOCK         1000
+#define NUM_CALL_BLOCKS_PER_THREAD  1000
 #define NUM_CALLS_PER_THREAD        (NUM_CALLS_PER_BLOCK * NUM_CALL_BLOCKS_PER_THREAD)
 
+int threadcount = 0;
 int err = 0;
 unsigned long *dataval = NULL;
 int writers = 0;
+int readers = 0;
 int write_lock_calls = 0;
 int read_lock_calls = 0;
+OSSL_TIME reader_end = { 0 };
+OSSL_TIME writer_end = { 0 };
 
 CRYPTO_RWLOCK *lock = NULL;
 
@@ -31,6 +35,7 @@ void do_rw_wlock(size_t num)
 {
     int i;
     unsigned long *newval, *oldval;
+    int local_write_lock_calls = 0;
 
     for (i = 0; i < NUM_CALLS_PER_THREAD; i++) {
         newval = OPENSSL_malloc(sizeof(int));        
@@ -42,10 +47,16 @@ void do_rw_wlock(size_t num)
         oldval = dataval;
         dataval = newval;
         CRYPTO_THREAD_unlock(lock);
-        write_lock_calls += 2; /* lock and unlock */
+        local_write_lock_calls += 2; /* lock and unlock */
         OPENSSL_free(oldval);
     }
- 
+
+    CRYPTO_THREAD_write_lock(lock);
+    write_lock_calls += local_write_lock_calls;
+    writers--;
+    if (writers == 0)
+        writer_end = ossl_time_now();
+    CRYPTO_THREAD_unlock(lock); 
 }
 
 void do_rw_rlock(size_t num)
@@ -67,12 +78,15 @@ void do_rw_rlock(size_t num)
 
     CRYPTO_THREAD_write_lock(lock);
     read_lock_calls += local_read_lock_calls;
+    readers--;
+    if (readers == 0)
+        reader_end = ossl_time_now();
     CRYPTO_THREAD_unlock(lock);
 }
 
 void do_rwlocks(size_t num)
 {
-    if (num < writers)
+    if (num >= threadcount - writers)
         do_rw_wlock(num);
     else
         do_rw_rlock(num);
@@ -80,8 +94,8 @@ void do_rwlocks(size_t num)
 
 int main(int argc, char *argv[])
 {
-    int threadcount;
     OSSL_TIME duration;
+    OSSL_TIME start;
     uint64_t us;
     double avwcalltime;
     double avrcalltime;
@@ -123,7 +137,10 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    printf("Running rwlock test with %d writers and %d readers\n", writers, threadcount - writers);
+    readers = threadcount - writers;
+    printf("Running rwlock test with %d writers and %d readers\n", writers, readers);
+
+    start = ossl_time_now(); 
 
     if (!perflib_run_multi_thread_test(do_rwlocks, threadcount, &duration)) {
         printf("Failed to run the test\n");
@@ -135,20 +152,21 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    us = ossl_time2us(duration);
-
+    us = ossl_time2us(ossl_time_subtract(writer_end, start));
     avwcalltime = (double)us / (double)write_lock_calls;
-    avrcalltime = (double)us / (double)read_lock_calls;
 
-    printf("total write lock/unlock calls %d\n", write_lock_calls);
-    printf("total read lock/unlock calls %d\n", read_lock_calls);
+    printf("total write lock/unlock calls %d in %lf us\n", write_lock_calls, (double)us);
+
+    us = ossl_time2us(ossl_time_subtract(reader_end, start));
+    avrcalltime = (double)us / (double)read_lock_calls;
+    printf("total read lock/unlock calls %d %lf us\n", read_lock_calls, (double)us);
     if (terse)
         printf("%lf %lf\n", avwcalltime, avrcalltime);
     else {
-        printf("Average time per %d CRYPTO_THREAD_write_lock/unlock calls: %lfus\n",
-               NUM_CALLS_PER_BLOCK, avwcalltime);
-        printf("Average time per %d CRYPTO_THREAD_read_lock/unlock calls: %lfus\n",
-               NUM_CALLS_PER_BLOCK, avrcalltime);
+        printf("Average time per CRYPTO_THREAD_write_lock/unlock call pair: %lfus\n",
+               avwcalltime);
+        printf("Average time per CRYPTO_THREAD_read_lock/unlock call pair: %lfus\n",
+               avrcalltime);
     }
 
     CRYPTO_THREAD_lock_free(lock);
